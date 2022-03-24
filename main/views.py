bd898822adb02b6e6c6b0565df7b0b4053fdbab9
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-03-24 10:46:10 +0100

    Custom forms

--- main/views.py
+++ main/views.py
@@ -7,7 +7,6 @@ from .forms import CreateNewList
 from rest_framework import viewsets
 from .serializer import ToDoListSerializer, ItemSerializer
 
-
 """Typical views for django"""
 
 
@@ -17,6 +16,23 @@ def home(response):
 
 def database_list(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
     return render(response, "list.html", {"ls": ls})
 
 
@@ -28,7 +44,7 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
-        return HttpResponseRedirect("/%i" %t.id)
+        return HttpResponseRedirect("/%i" % t.id)
     else:
         form = CreateNewList()
     return render(response, "create.html", {"form": form})
