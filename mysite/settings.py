56da8277fc1b84d3e2eb8074fa7f5e376ae16323
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-03-25 12:37:09 +0100

    register app

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'register.apps.RegisterConfig',
+    'crispy_forms',
     'rest_framework',
 ]
 
@@ -125,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
